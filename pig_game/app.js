cb7d6b504627e6494d279429877fcbfe9d9b441c
Sameer Kulkarni <sameermk97@gmail.com>
2020-07-22 12:19:40 +0530

    app js update

--- pig_game/app.js
+++ pig_game/app.js
@@ -9,18 +9,25 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores= [0,0];
 roundScore = 0;
 activePlayer = 1;
-dice = Math.floor(Math.random() * 6) + 1;
 
-console.log(dice);
-
-document.querySelector('#current-' + activePlayer).textContent = dice; 
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+    var dice = Math.floor(Math.random() * 6) + 1;
+    console.log(dice);
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'img/dice-' + dice + '.png';
+    //document.querySelector('#current-' + activePlayer).textContent = dice;
+});
