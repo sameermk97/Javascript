b92d4bb8206b8092003cbd1ac84384a9a95c177c
Sameer Kulkarni <sameermk97@gmail.com>
2020-08-11 22:16:40 +0530

    app js updated

--- pig_game/app.js
+++ pig_game/app.js
@@ -10,61 +10,86 @@ GAME RULES:
 */
 
 var scores, roundScore, activePlayer;
+var gamePlaying = true;
 
-scores= [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 
 document.querySelector('.btn-roll').addEventListener('click',function(){
-    var activePLayer;
-    var dice = Math.floor(Math.random() * 6) + 1;
-    console.log(dice);
 
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'img/dice-' + dice + '.png';
+    if(gamePlaying){
+        var dice = Math.floor(Math.random() * 6) + 1;
+        console.log(dice);
 
-    if(dice != 1){
-        roundScore += dice;
-        document.getElementById('current-' + activePlayer).textContent = roundScore;
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'img/dice-' + dice + '.png';
+
+        if(dice != 1){
+            roundScore += dice;
+            document.getElementById('current-' + activePlayer).textContent = roundScore;
     
-    }
-    else{
-        alert("You rolled 1...Next player's turn");
+        }
+
+        else{
+            
+            alert("Sorry, you rolled 1 this time...Next player's turn");
        
-        nextPlayer();
-    }
+            nextPlayer();
+        }
         
+    }
+       
 });
 
 
 document.querySelector('.btn-hold').addEventListener('click',function(){
-    scores[activePlayer] += roundScore;
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    if(gamePlaying){
+        scores[activePlayer] += roundScore;
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
 
-    if (scores[activePlayer] >= 10){
-        document.querySelector('#name-'+ activePlayer).textContent = 'Winner';
-        document.querySelector('.dice').style.display = 'none';
-        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+        if (scores[activePlayer] >= 100){
+            document.querySelector('#name-'+ activePlayer).textContent = 'Winner';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
         
-    }
-    else{
-        nextPlayer();
-    }
+        }
+        else{
+            nextPlayer();
+        }   
 
+    }
     
 })
+
+document.querySelector('.btn-new').addEventListener('click',init);
+
+function init(){
+    gamePlaying = true;
+    scores= [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+
+}
 
 
 function nextPlayer(){
@@ -80,4 +105,5 @@ function nextPlayer(){
 
         document.querySelector('.dice').style.display = 'none';
 
+       
 }
