12d3cf946e3029603ba19f09fac132de5d66e933
Sameer Kulkarni <sameermk97@gmail.com>
2020-08-11 17:11:45 +0530

    app js updated

--- pig_game/app.js
+++ pig_game/app.js
@@ -23,6 +23,7 @@ document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
+
 document.querySelector('.btn-roll').addEventListener('click',function(){
     var activePLayer;
     var dice = Math.floor(Math.random() * 6) + 1;
@@ -39,7 +40,35 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
     }
     else{
         alert("You rolled 1...Next player's turn");
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+       
+        nextPlayer();
+    }
+        
+});
+
+
+document.querySelector('.btn-hold').addEventListener('click',function(){
+    scores[activePlayer] += roundScore;
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+
+    if (scores[activePlayer] >= 10){
+        document.querySelector('#name-'+ activePlayer).textContent = 'Winner';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+        
+    }
+    else{
+        nextPlayer();
+    }
+
+    
+})
+
+
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
         console.log(activePlayer + "actplay");    
         roundScore = 0;
 
@@ -50,5 +79,5 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
         document.querySelector('.player-1-panel').classList.toggle('active');
 
         document.querySelector('.dice').style.display = 'none';
-    }    
-});
+
+}
